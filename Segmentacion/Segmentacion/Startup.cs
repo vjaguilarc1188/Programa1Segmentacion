bf03a648c4e4bf6255def6f5faf15bb48a365efb
Victor <Victor>
2017-04-25 19:57:02 -0500

    version 1

---
+++ Segmentacion/Segmentacion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Segmentacion.Startup))]
+namespace Segmentacion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
